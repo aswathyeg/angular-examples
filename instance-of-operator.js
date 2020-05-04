adb34a64b102ad1faf93c62c9bf567f6bd1585d2
aswathyeg <aswathy.eg.bijesh@gmail.com>
2020-05-04 20:35:26 +0530

    instance of operator in js

---
+++ instance-of-operator.js
@@ -0,0 +1,8 @@
+var Students = /** @class */ (function () {
+    function Students() {
+    }
+    return Students;
+}());
+var obj = new Students();
+var isrollnumber = obj instanceof Students;
+console.log("obj instanceof Students?" + isrollnumber);
