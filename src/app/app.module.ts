6395fc3ff293ade8637929de2be90dd4e6f86311
aswathyeg <aswathy.eg.bijesh@gmail.com>
2020-05-05 22:03:40 +0530

    added new component typescript example

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,12 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { TypescriptExamplesComponent } from './typescript-examples/typescript-examples.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    TypescriptExamplesComponent
   ],
   imports: [
     BrowserModule,
